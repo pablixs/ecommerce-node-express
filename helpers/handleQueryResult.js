f5f33066122876be9d8a67c44f95f6ed2218db75
unknown <pablicjs@gmail.com>
2023-01-07 20:10:11 -0300

    explaining handler

--- helpers/handleQueryResult.js
+++ helpers/handleQueryResult.js
@@ -1,3 +1,7 @@
+//** Takes the success, 
+//** data & error from the return of the promise of the query, 
+//** handle it and resolve
+
 function handleQueryResult(success, data, error) {
     if(success){
         console.log(success)
