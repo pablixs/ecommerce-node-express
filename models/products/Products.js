5c0baf4ed2b0e3fd0968babc22ae95ec26255723
unknown <pablicjs@gmail.com>
2023-01-06 22:01:47 -0300

    creating new routes

--- models/products/Products.js
+++ models/products/Products.js
@@ -19,6 +19,51 @@ class Products {
             }
         }
     }
+
+    static async get_product_by_category(id){
+        const data = await query('SELECT * FROM products WHERE category_id = ?',[id]);
+        try {
+            if (data.length === 0) return {
+                success: false,
+                error: 'No se encontraron categorias'
+            }
+
+            return {
+                success: true,
+                data
+            }
+            
+        } catch (error) {
+            return {
+                success: false,
+                error
+            }            
+        }
+    }
+
+
+    static async new_product(name, short_description, category_id, stock, price){
+        const convert = () =>{
+            return name.toLowerCase().replace(/[á]/g, 'a').replace(/[é]/g, 'e').replace(/[í]/g, 'i').replace(/[ó]/g, 'o').replace(/[ú]/g, 'u').replace(/[\s]/g, '-').replace(/[^\w ]+/g, '-');
+        }
+        console.log(convert())
+        const data = await query('INSERT INTO products(name,name_url,short_description,category_id,stock,price) VALUES (?,?,?,?,?,?)',[name,convert(), short_description, category_id, stock, price])
+        try {
+            if (data.length === 0) return {
+                success: false,
+                error: 'No se encontraron categorias'
+            }
+            return {
+                success: true,
+                data
+            }
+        } catch (error) {
+            return {
+                success: false,
+                error
+            }
+        }
+    }
 };
 
 module.exports = Products;
--- routes/products_routes/products.routes.js
+++ routes/products_routes/products.routes.js
@@ -3,13 +3,32 @@ const router = express.Router();
 const cookie = require('cookie-parser');
 
 const Products = require('../../models/products/Products.js');
-const { query } = require('express');
 
 // const passport = require('passport');
 
 // router.use(passport.initialize());
 
 // require('../middlewares/auth');
+
+
+//* crud */
+
+router.post('/newproduct', async(req,res) => {
+    const { name, short_description, category_id, stock, price } = req.body;
+
+    const { success, data, error } = await Products.new_product(name, short_description, category_id, stock, price);
+
+    if(success){
+        res.send(data)
+    } else {
+        res.sendStatus(404).send({
+            "message": "No se pudo añadir el nuevo producto",
+            error
+        })
+    }
+})
+
+//*  */
 
 router.get('/', async (req ,res) => {
     const { success, data, error } = await Products.get_categories()
@@ -33,10 +52,47 @@ router.get('/:category', async (req,res) => {
 
     if(success){
 
-        const conicidences = categories.findIndex(categories => categories.name === query_category );
-        console.log(conicidences)
-        if(conicidences !== -1){
-            res.send({'Esta es la categoria que coincide': categories[conicidences]})
+        const coincidences = categories.findIndex(categories => categories.name === query_category );
+        console.log(coincidences)
+        if(coincidences !== -1){
+            const { success:newSuccess, data:products, error:newError } = await Products.get_product_by_category(categories[coincidences].id)
+            res.send({
+                'Esta es la categoria que coincide': categories[coincidences],
+                'Estos son los productos que coinciden con la categoria': products
+            })
+
+        } else {
+            res.send({"message": "No hubo categorias con ese nombre"})
+        }
+
+
+    } else {
+        res.status(404).send(error);
+    }
+})
+
+router.get('/:category/:product', async(req,res) => {
+    const { success, data:categories, error } = await Products.get_categories()
+    console.log(req.params)
+    console.log('******************')
+    console.log(categories)
+
+    const { category:query_category, product:query_product } = req.params;
+    // console.log(query_category)
+
+    console.log(query_category)
+
+    if(success){
+        const coincidences = categories.findIndex(categories => categories.name === query_category );
+        console.log(coincidences)
+        if(coincidences !== -1){
+            const { success:newSuccess, data:product, error:newError } = await Products.get_product_by_category(categories[coincidences].id)
+            console.log(product)
+            const final_product = product.findIndex(product => product.name_url === query_product);             
+            res.send({
+                'Esta es la categoria que coincide': categories[coincidences],
+                'Estos son los productos que coinciden con la url': product[final_product]
+            })
 
         } else {
             res.send({"message": "No hubo categorias con ese nombre"})
